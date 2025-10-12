ad158660b67748e729ecead99b9f6561095885aa
FKBGRENSHE <fkbgrenshe@163.com>
2025-10-12 11:25:35 +0800

    update for midterm pre

---
+++ module/featurePreprocess.py
@@ -0,0 +1,8 @@
+## Feature Preprocessing Module
+
+## 数据导入
+import pandas as pd
+import numpy as np
+
+train = pd.read_csv(r'D:\Project\NusSemester1BigDataProjectBETH\G9Proj\data\sample_processes_train.csv')  # 读取csv文件
+test = pd.read_csv(r'D:\Project\NusSemester1BigDataProjectBETH\G9Proj\data\sample_processes_test.csv')  # 读取csv文件
